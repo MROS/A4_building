aedb82c5741153c2c262f517067f3b5bb3c89b50
MROS <yc1043@gmail.com>
2014-12-20 07:53:11 +0800

    table can rank

--- table.js
+++ table.js
@@ -3,7 +3,7 @@ var data = [
 	["第一次分數", "", "", ""],
 	["第二次分數", "", "", ""],
 	["第三次分數", "", "", ""],
-	["第四次分數", 45.5, "", ""],
+	["第四次分數", "", "", ""],
 	["第五次分數", "", "", ""],
 	["最佳分數", "", "", ""],
 	["目前排名", "", "", ""],
@@ -24,38 +24,36 @@ var table = new Handsontable(container,
 		});
 
 table.addHook('afterChange', function(changes, source){
-
 				if (changes[0][0] > 5 || changes[0][0] < 1) {return;}
-
 				// 算最大值
 				for(var teams = teamsStart; teams < data[0].length; teams++) {
 					var max = 0;
 					for (var times = timesStart; times < timesStart + MAX_TIMES; times++) {
-						console.log(times);
 						var score = table.getDataAtCell(times, teams);
-						if (typeof(score)) {
+						score = parseFloat(score)
+						console.log(score);
+						if (score != NaN) {
 							max = max < score ? score : max;
 						}
 					};
+					console.log("max is " + max);
 					table.setDataAtCell(6, teams, max);
 				}
 				// 排名
 				var rankRow = 7;
 				for(var teams = teamsStart; teams < data[0].length; teams++) {
-					rank = 0;
+					rank = 1;
 					for(var enemy = teamsStart; enemy < data[0].length; enemy++) {
-						if (table.getDataAtCell(maxRow, teams) < table.)
+						if (table.getDataAtCell(maxRow, teams) < table.getDataAtCell(maxRow, enemy)) {
+							rank++;
+						}
 					}
+					table.setDataAtCell(rankRow, teams, rank);
 				}
 				// self.updateSettings({data: data});
 			})
 
 var add_group = document.getElementById('add_group');
 add_group.onclick = function(){
-	data[0].push("新隊伍名");
-	for (var i = 1; i < data.length; i++) {
-		data[i].push("");
-	}
-	table.updateSettings({data: data});
-	console.log(data);
+	table.alter('insert_col', table.countCols);
 }
